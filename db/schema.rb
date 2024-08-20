4d405bcd61ebab96c99ae4a486adcc9c4641a047
leesb <echo3042@gmail.com>
2024-08-19 18:30:58 -0700

    update schema and seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_16_151734) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_17_213102) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_16_151734) do
     t.datetime "updated_at", null: false
     t.index ["bearer_id", "bearer_type"], name: "index_api_keys_on_bearer_id_and_bearer_type"
     t.index ["token_digest"], name: "index_api_keys_on_token_digest", unique: true
+  end
+
+  create_table "geolocations", force: :cascade do |t|
+    t.string "ip_address"
+    t.string "country"
+    t.string "region"
+    t.string "city"
+    t.decimal "lat", precision: 10, scale: 6
+    t.decimal "lon", precision: 10, scale: 6
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ip_address"], name: "unique_ip_address", unique: true
   end
 
   create_table "users", force: :cascade do |t|
